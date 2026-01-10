91d170d3160fa46661b24ef5233554aedb96da17
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-01-10 17:09:11 +0300

    Added Models Layer with userDTO

---
+++ Backend/ModelsLayer/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelsLayer
+{
+    public class UserDTO
+    {
+        public int UserID { get; set; }
+        public int PersonID { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
