48b48799c5bcab3b2a290e45afbe1b381dca6c72
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-01-20 15:38:26 +0300

    Created Driver DTO

---
+++ Backend/ModelsLayer/DriverDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLayer
+{
+    public class DriverDTO
+    {
+        public int DriverID { get; set; }
+        public int PersonID { get; set; }
+        public int CreatedByUserID { get; set; }
+        public string CreatedDate { get; set; }
+
+        public DriverDTO(int driverId, int personId, int createdId, string createdDate)
+        {
+            this.DriverID = driverId;
+            this.PersonID = personId;
+            this.CreatedByUserID = createdId;
+            this.CreatedDate = createdDate;
+        }
+    }
+}
