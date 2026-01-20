0f18d9b1dc55c79560c3836b5484e269685d872e
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-01-20 16:49:55 +0300

    Created DriverBusinessLayer Functionalities

---
+++ Backend/DLMBusinessLayer/clsDrive.cs
@@ -0,0 +1,39 @@
+﻿using DLMDataLayer;
+using ModelsLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMBusinessLayer
+{
+    public class clsDrive
+    {
+        public int CreateDriverDTO(CreateDriverDTO newDriver)
+        {
+
+            if(newDriver == null)
+            {
+                return -1;
+            }
+
+            return clsDriverDataAccess.CreateDriver(newDriver);
+        }
+
+        public DriverDTO FindDriver(int personId)
+        {
+            return clsDriverDataAccess.FindDriver(personId);
+        }
+
+        public int GetDriverID(int personId)
+        {
+            return clsDriverDataAccess.GetDriverID(personId);
+        }
+
+        public List<DriverDTO> GetDrivers()
+        {
+            return clsDriverDataAccess.GetDrivers();
+        }
+    }
+}
