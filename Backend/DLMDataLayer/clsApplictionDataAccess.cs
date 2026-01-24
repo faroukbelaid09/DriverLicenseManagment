2b266f58383fd0d72748eb8e04adf5f6abd7f5bd
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-01-24 19:12:23 +0300

    Created application data access layer functionalities

---
+++ Backend/DLMDataLayer/clsApplictionDataAccess.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Data.SqlClient;
+using ModelsLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMDataLayer
+{
+    public class clsApplicationDataAccess
+    {
+        public static List<LocalApplicationDTO> GetAllLocalApplications()
+        {
+            List<LocalApplicationDTO> localApplications = new List<LocalApplicationDTO>();
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetAllLocalApplications", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            localApplications.Add(new LocalApplicationDTO(
+                                reader.GetInt32(reader.GetOrdinal("LocalDrivingLicenseApplicationID")),
+                                reader.GetString(reader.GetOrdinal("DrivingClass")),
+                                reader.GetString(reader.GetOrdinal("NationalNo")),
+                                reader.GetString(reader.GetOrdinal("FullName")),
+                                reader.GetDateTime(reader.GetOrdinal("ApplicationDate")),
+                                reader.GetInt32(reader.GetOrdinal("PassedTests")),
+                                reader.GetInt32(reader.GetOrdinal("ApplicationStatus"))
+                                ));
+                        }
+                    }
+                }
+            }
+            return localApplications;
+        }
+
+        public static bool CheckIfApplicationExist(string nationalNo, string drivingClass)
+        {
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("CheckIfApplicationExist", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@nationalNo", nationalNo);
+                    cmd.Parameters.AddWithValue("@drivingClass", drivingClass);
+
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // If there's at least one row, the application exists
+                        return reader.HasRows;
+                    }
+                }
+            }
+        }
+
+        public static ApplicationDTO FindApplication(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("FindApplication", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@appID", id);
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new ApplicationDTO
+                                (
+                                reader.GetInt32(reader.GetOrdinal("ApplicationID")),
+                                reader.GetInt32(reader.GetOrdinal("ApplicantPersonID")),
+                                reader.GetDateTime(reader.GetOrdinal("ApplicationDate")),
+                                reader.GetInt32(reader.GetOrdinal("ApplicationTypeID")),
+                                reader.GetInt32(reader.GetOrdinal("ApplicationStatus")),
+                                reader.GetDateTime(reader.GetOrdinal("LastStatusDate")),
+                                reader.GetInt32(reader.GetOrdinal("PaidFees")),
+                                reader.GetInt32(reader.GetOrdinal("CreatedByUserID"))
+                                );
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static int CreateApplication(CreateApplicationDTO createApplicationDTO)
+        {
+            int applicationID = -1;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("CreateApplication", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@ApplicantPersonID", createApplicationDTO.ApplicantPersonID);
+                        cmd.Parameters.AddWithValue("@ApplicationDate", createApplicationDTO.ApplicationDate);
+                        cmd.Parameters.AddWithValue("@ApplicationTypeID", createApplicationDTO.ApplicationTypeID);
+                        cmd.Parameters.AddWithValue("@ApplicationStatus", createApplicationDTO.ApplicationStatus);
+                        cmd.Parameters.AddWithValue("@LastStatusDate", createApplicationDTO.LastStatusDate);
+                        cmd.Parameters.AddWithValue("@PaidFees", createApplicationDTO.PaidFees);
+                        cmd.Parameters.AddWithValue("@CreatedByUserID", createApplicationDTO.CreatedByUserID);
+                        conn.Open();
+
+                        applicationID = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return applicationID;
+        }
+
+        public static bool UpdateApplicationStatus(UpdateApplicationDTO updateApplicationDTO)
+        {
+            bool isUpdated = false;
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateApplicationStatus", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@appID", updateApplicationDTO.ApplicationID);
+                    cmd.Parameters.AddWithValue("@appStatus", updateApplicationDTO.ApplicationStatus);
+
+                    conn.Open();
+
+                    int rowAffected = cmd.ExecuteNonQuery();
+
+                    if (rowAffected > 0)
+                    {
+                        isUpdated = true;
+                    }
+                }
+            }
+
+            return isUpdated;
+        }
+
+    }
+}
--- Backend/ModelsLayer/ApplicationDTO.cs
+++ Backend/ModelsLayer/ApplicationDTO.cs
@@ -8,6 +8,32 @@ namespace ModelsLayer
 {
     public class ApplicationDTO
     {
+        public int ApplicationID { get; set; }
+        public int ApplicantPersonID { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public int ApplicationTypeID { get; set; }
+        public int ApplicationStatus { get; set; }
+        public DateTime LastStatusDate { get; set; }
+        public int PaidFees { get; set; }
+        public int CreatedByUserID { get; set; }
+
+        public ApplicationDTO(int ApplicationID, int ApplicantPersonID, DateTime ApplicationDate, int ApplicationTypeID,
+            int ApplicationStatus, DateTime LastStatusDate, int PaidFees, int CreatedByUserID)
+        {
+            this.ApplicationID = ApplicationID;
+            this.ApplicantPersonID = ApplicantPersonID;
+            this.ApplicationDate = ApplicationDate;
+            this.ApplicationTypeID = ApplicationTypeID;
+            this.ApplicationStatus = ApplicationStatus;
+            this.LastStatusDate = LastStatusDate;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserID = CreatedByUserID;
+        }
+    }
+
+
+    public class LocalApplicationDTO
+    {
         public int LocalDrivingLicenseApplicationID { get; set; }
         public string DrivingClass { get; set; }
         public string NationalNo { get; set; }
@@ -16,7 +42,7 @@ namespace ModelsLayer
         public int PassedTests { get; set; }
         public int ApplicationStatus {  get; set; }
 
-        public ApplicationDTO(int LocalDrivingLicenseApplicationID, string DrivingClass, string NationalNo,
+        public LocalApplicationDTO(int LocalDrivingLicenseApplicationID, string DrivingClass, string NationalNo,
             string FullName, DateTime ApplicationDate, int PassedTests, int ApplicationStatus)
         {
             this.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
@@ -26,6 +52,30 @@ namespace ModelsLayer
             this.ApplicationDate = ApplicationDate;
             this.PassedTests = PassedTests;
             this.ApplicationStatus = ApplicationStatus;
+        }
+    }
+
+
+    public class CreateApplicationDTO
+    {
+        public int ApplicantPersonID { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public int ApplicationTypeID { get; set; }
+        public int ApplicationStatus { get; set; }
+        public DateTime LastStatusDate { get; set; }
+        public int PaidFees { get; set; }
+        public int CreatedByUserID { get; set; }
+
+        public CreateApplicationDTO(int ApplicantPersonID, DateTime ApplicationDate, int ApplicationTypeID,
+            int ApplicationStatus, DateTime LastStatusDate, int PaidFees, int CreatedByUserID)
+        {
+            this.ApplicantPersonID = ApplicantPersonID;
+            this.ApplicationDate = ApplicationDate;
+            this.ApplicationTypeID = ApplicationTypeID;
+            this.ApplicationStatus = ApplicationStatus;
+            this.LastStatusDate = LastStatusDate;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserID = CreatedByUserID;
         }
     }
 
