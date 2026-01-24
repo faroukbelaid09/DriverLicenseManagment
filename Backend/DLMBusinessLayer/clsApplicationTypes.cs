e898e74cfe833590ba95db08770d3f861cf2128e
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-01-24 13:20:24 +0300

    Created Application Types business layer functionalities

---
+++ Backend/DLMBusinessLayer/clsApplicationTypes.cs
@@ -0,0 +1,29 @@
+﻿using DLMDataLayer;
+using ModelsLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMBusinessLayer
+{
+    public class clsApplicationTypes
+    {
+        public static ApplicationTypeDTO GetApplicationTypeById(int appTypeId)
+        {
+            return clsApplicationTypesDataAccess.GetApplicationTypeById(appTypeId);
+        }
+
+        public static bool UpdateApplicationTypes(ApplicationTypeDTO applicationTypeDTO)
+        {
+            return clsApplicationTypesDataAccess.UpdateApplicationType(applicationTypeDTO);
+        }
+
+        public static List<ApplicationTypeDTO> GetAllApplicationTypes() 
+        {
+            return clsApplicationTypesDataAccess.GetAllApplicationTypes();
+        }
+    }
+}
