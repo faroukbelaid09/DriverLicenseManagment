423d71a09e775d158192ef99af11adaddb30cccb
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-01-31 17:23:39 +0300

    Created license class DTO and License class Data access layer functionalties

---
+++ Backend/DLMDataLayer/clsLicenseClassDataAccess.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Data.SqlClient;
+using ModelsLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMDataLayer
+{
+    public class clsLicenseClassDataAccess
+    {
+        public List<LicenseClassDTO> GetLicenseClasses()
+        {
+            List<LicenseClassDTO> licenseClasses = new List<LicenseClassDTO>();
+
+            using(SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using(SqlCommand cmd = new SqlCommand("GetLicenseClasses", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            licenseClasses.Add(new LicenseClassDTO(
+                                reader.GetInt32(reader.GetOrdinal("LicenseClassID")),
+                                reader.GetString(reader.GetOrdinal("ClassName")),
+                                reader.GetString(reader.GetOrdinal("ClassDescription")),
+                                reader.GetInt32(reader.GetOrdinal("MinimumAllowedAge")),
+                                reader.GetInt32(reader.GetOrdinal("DefaultValidityLength")),
+                                reader.GetInt32(reader.GetOrdinal("ClassFees"))
+                                ));
+                        }
+                    }
+                }
+            }
+
+            return licenseClasses;
+        }
+
+
+        public LicenseClassDTO GetLicenseClassByName(string className) 
+        {
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetClassByName", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@className", className);
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            return new LicenseClassDTO(
+                                reader.GetInt32(reader.GetOrdinal("LicenseClassID")),
+                                reader.GetString(reader.GetOrdinal("ClassName")),
+                                reader.GetString(reader.GetOrdinal("ClassDescription")),
+                                reader.GetInt32(reader.GetOrdinal("MinimumAllowedAge")),
+                                reader.GetInt32(reader.GetOrdinal("DefaultValidityLength")),
+                                reader.GetInt32(reader.GetOrdinal("ClassFees"))
+                                );
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    
+    
+        public List<string> GetClassesNames()
+        {
+            List<string> classesNames = new List<string>();
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetLicenseClasses", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            classesNames.Add(
+                                reader.GetString(reader.GetOrdinal("ClassName"))
+                                );
+                        }
+                    }
+                }
+            }
+
+            return classesNames;
+        }
+    }
+}
---
+++ Backend/ModelsLayer/LicenseClassDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLayer
+{
+    public class LicenseClassDTO
+    {
+        public int LicenseClassID { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public int MinimumAllowedAge { get; set; }
+        public int DefaultValidityLength { get; set; }
+        public int ClassFees { get; set; }
+
+        public LicenseClassDTO(int LicenseClassID, string ClassName, string ClassDescription, int MinimumAllowedAge
+            , int DefaultValidityLength, int ClassFees) 
+        {
+            this.LicenseClassID = LicenseClassID;
+            this.ClassName = ClassName;
+            this.MinimumAllowedAge = MinimumAllowedAge;
+            this.DefaultValidityLength = DefaultValidityLength;
+            this.ClassFees = ClassFees;
+            this.ClassDescription = ClassDescription;
+        }
+    }
+}
